40ae41aba8c405e995fac0f487befe79db0ff527
elingeyes <yesmani.fa@gmail.com>
2026-07-13 23:39:30 -0400

    Personaje con detener y seguir

--- core/src/main/java/io/univalle/guardianes_demo/Main.java
+++ core/src/main/java/io/univalle/guardianes_demo/Main.java
@@ -23,62 +23,77 @@ public class Main extends ApplicationAdapter {
     private float velocidad = 200f;
     private float ancho = 50;
     private float alto = 70;
-
+    
     //Animación
     private int frameActual = 0;
     private float tiempoAnimacion = 0;
     private float tiempoEntreFrames = 0.15f;
+    private boolean seEstaMoviendo = false;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-
+        
         fondo = new Texture("background.png");
-
+        
         personajeWalk = new Texture[4];
         personajeWalk[0] = new Texture("characters/player/guardian/walk/walk_1.png");
         personajeWalk[1] = new Texture("characters/player/guardian/walk/walk_2.png");
         personajeWalk[2] = new Texture("characters/player/guardian/walk/walk_3.png");
         personajeWalk[3] = new Texture("characters/player/guardian/walk/walk_4.png");
-
+        
         personajeActual = personajeWalk[0];
     }
 
     @Override
     public void render() {
         float deltaTime = Gdx.graphics.getDeltaTime();
-
+        
         handleInput(deltaTime);
         updateAnimation(deltaTime);
-
+        
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         batch.draw(fondo, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(personajeActual, posXPersonaje, posYPersonaje, ancho, alto);
         batch.end();
     }
-
+    
     private void handleInput(float deltaTime) {
+        seEstaMoviendo = false;
+        
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             posXPersonaje -= velocidad * deltaTime;
+            seEstaMoviendo = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             posXPersonaje += velocidad * deltaTime;
+            seEstaMoviendo = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             posYPersonaje += velocidad * deltaTime;
+            seEstaMoviendo = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             posYPersonaje -= velocidad * deltaTime;
+            seEstaMoviendo = true;
+        }
+        
+        if (!seEstaMoviendo) {
+            frameActual = 0;
+            tiempoAnimacion = 0;
+            personajeActual = personajeWalk[0];
         }
     }
-
+    
     private void updateAnimation(float deltaTime) {
-        tiempoAnimacion += deltaTime;
-        if (tiempoAnimacion >= tiempoEntreFrames) {
-            tiempoAnimacion = 0;
-            frameActual = (frameActual + 1) % 4;
-            personajeActual = personajeWalk[frameActual];
+        if (seEstaMoviendo) {
+            tiempoAnimacion += deltaTime;
+            if (tiempoAnimacion >= tiempoEntreFrames) {
+                tiempoAnimacion = 0;
+                frameActual = (frameActual + 1) % 4;
+                personajeActual = personajeWalk[frameActual];
+            }
         }
     }
 
